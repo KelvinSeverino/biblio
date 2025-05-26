609a2f185b10ed8332fe4087dde7c12c263584f1
Kelvin Severino <kseveri@outlook.com>
2025-05-26 16:47:40 -0300

    feat: CRUD Autor

---
+++ backend/app/Http/Controllers/Api/AutorController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Autor;
+use Illuminate\Http\Request;
+
+class AutorController extends Controller
+{
+    public function index()
+    {
+        return Autor::all();
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validated();
+
+        $autor = Autor::create($data);
+
+        return response()->json($autor, 201);
+    }
+
+    public function show($id)
+    {
+        return Autor::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $autor = Autor::findOrFail($id);
+
+        $autor->update($request->validated());
+
+        return response()->json($autor);
+    }
+
+    public function destroy($id)
+    {
+        $autor = Autor::findOrFail($id);
+        $autor->delete();
+        return response()->json(null, 204);
+    }
+}
