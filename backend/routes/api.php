ab2b1eeceeb075cda851f0dfe0ce338e6fab2c13
Kelvin Severino <kseveri@outlook.com>
2025-05-26 16:46:43 -0300

    build: include api

---
+++ backend/routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\{
+    AssuntoController,
+    AutorController,
+    LivroController
+};
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::apiResource('livros', LivroController::class);
+Route::apiResource('autors', AutorController::class);
+Route::apiResource('assuntos', AssuntoController::class);
