3a3cba8b2502387cdb129a99ac7f7b89a96201da
Kelvin Severino <kseveri@outlook.com>
2025-06-09 20:33:40 -0300

    refactor: ajuste no layout da tela AuthorCreate

--- frontend/src/components/Authors/AuthorCreate.jsx
+++ frontend/src/components/Authors/AuthorCreate.jsx
@@ -11,26 +11,28 @@ const AuthorCreate = () => {
     } = useAuthorForm(); 
 
     return (
-        <div className="container">
-            <Header />
-            <div className='row pt-4'>
-                <div className='col-md-12'>
-                    <h3>Informe Dados do Autor</h3>
-                    <form onSubmit={handleSubmit}>
-                        {successMessage && <div className="alert alert-success">{successMessage}</div>}
-                        {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-                        <div className='row'>
-                            <div className='col-12'>
-                                <div className="mt-2">
-                                    <label className="form-label">Nome:</label>
-                                    <input type="text" className="form-control" id="nome" placeholder="Insira nome" name="nome" value={authorField.nome} onChange={handleChange} />
+        <div>
+            <Header />                
+            <div className="container">            
+                <div className='row pt-4'>
+                    <div className='col-md-12'>
+                        <h3>Informe Dados do Autor</h3>
+                        <form onSubmit={handleSubmit}>
+                            {successMessage && <div className="alert alert-success">{successMessage}</div>}
+                            {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+                            <div className='row'>
+                                <div className='col-12'>
+                                    <div className="mt-2">
+                                        <label className="form-label">Nome:</label>
+                                        <input type="text" className="form-control" id="nome" placeholder="Insira nome" name="nome" value={authorField.nome} onChange={handleChange} />
+                                    </div>
                                 </div>
-                            </div>
-                        </div>    
-                        
-                        <br />
-                        <button type="submit" className="btn btn-success">Gravar</button>                             
-                    </form>
+                            </div>    
+                            
+                            <br />
+                            <button type="submit" className="btn btn-success">Gravar</button>                             
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
